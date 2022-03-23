5e953f54d8e62e237fa3ce7387f661e0cd8e4c1f
MARINE GUEGUEN <gueguenmarine.p7@gmail.com>
2022-03-23 15:40:50 +0100

    actualization for quest 2

--- thanks.php
+++ thanks.php
@@ -7,10 +7,70 @@
     <title>Thanks</title>
 </head>
 <body>
-<p>Merci <?= $_POST['firstName'];?> <?= $_POST['lastName'];?> de nous avoir contacté à propos de “<?= $_POST['topic'];?>”.
+<?php
+$nameErr = $topicErr = $phoneErr = $emailErr = $messageErr = "";
+$firstName = $lastName = $topic = $phone = $email = $message = "";
 
-Un de nos conseiller vous contactera soit à l’adresse <?= $_POST['email'];?> ou par téléphone au <?= $_POST['phone'];?> dans les plus brefs délais pour traiter votre demande : 
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 
-<?= $_POST['message'];?></p>
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (empty($_POST['firstName']) || empty($_POST['lastName'])) {
+        $nameErr = "Merci de renseigner votre nom complet. <br>";
+    } else {
+        $firstName = test_input($_POST["firstName"]);
+        $lastName = test_input($_POST["lastName"]);
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$firstName)) {
+        $nameErr = "Seules des lettres et des espaces sont autorisés dans votre prénom.<br>";
+        } 
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$lastName)) {
+            $nameErr = "Seules des lettres et des espaces sont autorisés dans votre nom.<br>";
+        } 
+    } 
+    
+    if (empty($_POST["email"])) {
+        $emailErr = "Merci de renseigner votre email.<br>";
+      } else {
+        $email = test_input($_POST["email"]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Merci de renseigner un email valide.<br>";
+        }
+    }
+
+    if (empty($_POST["phone"])) {
+        $phoneErr = "Merci de renseigner votre numéro de téléphone.<br>";
+    } else {
+        $phone = test_input($_POST["phone"]);
+        if(!preg_match("#[0][1-9][- \.?]?([0-9][0-9][- \.?]?){4}$#", $phone)) {
+            $phoneErr = "Merci de renseigner un numéro de téléphone valide.<br>";
+        }
+    }
+
+    if (empty($_POST["topic"])) {
+        $topicErr = "Veuillez choisir une catégorie.<br>";
+    } else {
+        $topic = test_input($_POST["topic"]);
+    }
+
+    if (empty($_POST["message"])) {
+        $messageErr = "Veuillez écrire votre message.<br>";
+    } else {
+        $message = test_input($_POST["message"]);
+    }
+
+    if (empty($nameErr) && empty($topicErr) && empty($phoneErr) && empty($emailErr) && empty($messageErr = "")) {
+        echo "Merci " . htmlspecialchars($_POST['firstName']) . " " . htmlspecialchars($_POST['lastName']) . " de nous avoir contacté à propos de “" . htmlspecialchars($_POST['topic']) . "”. <br>Un de nos conseiller vous contactera soit à l’adresse " . htmlspecialchars($_POST['email']) . " ou par téléphone au " . htmlspecialchars($_POST['phone']) . "dans les plus brefs délais pour traiter votre demande :<br>" . htmlspecialchars($_POST['message']);
+    } else {
+        echo $nameErr, $topicErr, $emailErr, $phoneErr, $messageErr;
+    }
+
+    
+}
+
+?>
 </body>
 </html>
